fd2e1175e3634446927e74e3d94ef685c529f74a
Vova <vl788704@gmail.com>
2020-09-26 12:43:49 +0300

    New version

--- searcher.py
+++ searcher.py
@@ -7,21 +7,54 @@ def is_domain_part_valid(part):
 
 
 
-def domain_list_generator(domain_name):
+def domain_list_generator(domain_names):
     combinations = combinations_with_replacement(string.ascii_lowercase + '-', 3)
     result = []
+    # read_last_domain_name()
+    found_last_domain_name = False
+    last_domain_name = read_last_domain_name()
+    print(last_domain_name)
     for combination in combinations:
         if is_domain_part_valid(combination):
-            result.append(''.join(combination) + "." + domain_name)
+            for domain_name in domain_names:
+                domain = (''.join(combination) + "." + domain_name)
+                if last_domain_name == domain or last_domain_name == False:
+                    found_last_domain_name = True
+                    continue
+                if not found_last_domain_name:
+                    continue
+                result.append(domain)
     return result
 
+def save_last_domain_name(domainname: str):
+    with open("lastdomainname.txt", 'w') as f:
+        f.write(domainname)
+
+
+def read_last_domain_name():
+    with open("lastdomainname.txt", 'r') as f:
+        last_domain_name = f.read()
+        if len(last_domain_name.strip()) > 0:
+            return last_domain_name
+        return False
+
+
+def save_domain_name(domain):
+    with open('free_domains.lst', 'a+') as f:
+        f.write(domain + '\n')
 
 def domain_list_validator(domain_list):
     result = []
     re_expired_domain = re.compile(r'registrar|Expiry Date|Creation Date', flags=re.IGNORECASE)
+    #read_last_domain_name()
     for domain in domain_list:
+        print(f'checking domain name:{domain}')
         if is_free_domain_name(domain, re_expired_domain):
-            print(domain)
+            print(f'free domain:{domain}!')
+        #else:
+        #    print("bbb.cc" + domain)
+            save_last_domain_name(domain)
+            save_domain_name(domain)
             result.append(domain)
     return result
 
@@ -31,11 +64,11 @@ def is_free_domain_name(domainname: str, re_expired_domain):
     output = str(d.stdout)
     return not re_expired_domain.findall(output)
 
-
 if __name__ == "__main__":
     # execute only if run as a script
-    domain_list = domain_list_generator('io')
-    free_domains = domain_list_validator(domain_list)
+    list = ['cc', 'us', 'vc', 'io', 'co', 'me', 'im']
+    domain_list = domain_list_generator(list)
     print(domain_list)
+    free_domains = domain_list_validator(domain_list)
 
 
