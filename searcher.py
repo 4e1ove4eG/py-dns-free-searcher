9db2c90cc0c7636c7b844d6e0c19b8ab1ca2e285
vlebedev <vl788704@gmail.com>
2020-10-11 22:36:05 +0300

    created functions load_string_from_file() and load_string_from_file()

--- searcher.py
+++ searcher.py
@@ -3,7 +3,7 @@ import string
 import subprocess
 from itertools import combinations_with_replacement
 
-
+# TODO обработка whois таймаут
 # TODO: вынести в отдельный модуль и имплементировать класс сохранения и извлечения строки из файла
 class StringPersistence:
     string = ""
@@ -20,11 +20,13 @@ class StringPersistence:
 def is_domain_part_valid(part):
     return part[0] != '-' and part[-1] != '-'
 
-# TODO: добавить функционал востановления с места последней остановки и сохранения текущего состояния
-def domain_list_generator(domain_name: str, string_persistence: StringPersistence = None):
+
+# TODO: добавить функционал востановления с меcта последней остановки и сохранения текущего состояния
+def domain_list_generator(domain_name: str, start: str = None):
     combinations = combinations_with_replacement(string.ascii_lowercase + '-', 3)
     result = []
     for combination in combinations:
+#        if
         if is_domain_part_valid(combination):
             result.append(''.join(combination) + "." + domain_name)
     # TODO: заменить на yield и рассказать про генераторы
@@ -33,14 +35,30 @@ def domain_list_generator(domain_name: str, string_persistence: StringPersistenc
 
 # TODO: перененести domain_list_validator и is_free_domain_name в отдельный модуль проверки DNS
 
-def domain_list_validator(domain_list):
+def domain_list_validator(dns_list, on_each_iteration_callback):
     result = []
     re_expired_domain = re.compile(r'registrar|Expiry Date|Creation Date', flags=re.IGNORECASE)
-    for domain in domain_list:
+    for domain in dns_list:
         if is_free_domain_name(domain, re_expired_domain):
             print(domain)
             result.append(domain)
+        on_each_iteration_callback(domain)
     return result
+
+
+def load_string_from_file():
+    filename = "last_domain.txt"
+    with open(filename, 'r') as file:
+        data = file.read()
+    return data
+
+
+def load_string_from_file(string: str):
+    print(string)
+    filename = "last_domain.txt"
+    with open(filename, "w") as text_file:
+        print(f"{string}", file=text_file)
+    return None
 
 
 def is_free_domain_name(domainname: str, re_expired_domain):
@@ -51,6 +69,8 @@ def is_free_domain_name(domainname: str, re_expired_domain):
 
 if __name__ == "__main__":
     # execute only if run as a script
-    domain_list = domain_list_generator('io')
-    free_domains = domain_list_validator(domain_list)
+    start = load_string_from_file()
+    print("Start from previous state: " + start)
+    domain_list = domain_list_generator('io', start)
+    free_domains = domain_list_validator(domain_list, save_string_to_file)
     print(domain_list)
